7be7f201d8f72eb3a2aa1dbf7f1e62eb9545208a
unknown <glebnikonob093@gmail.com>
2026-05-06 18:49:32 +0300

    debug: add Telegram webview boot fallback diagnostics

--- bot/webapp/src/main.tsx
+++ bot/webapp/src/main.tsx
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import { App } from "./App";
 import "./styles.css";
 
+// Signal successful bundle execution for Telegram WebView diagnostics.
+(window as any).__APP_BOOTED__ = true;
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
